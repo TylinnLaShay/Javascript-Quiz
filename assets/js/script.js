472a305aa04f505995e5037b2dca0eb7bec45ef5
TylinnLaShay <tylinnhall@gmail.com>
2021-07-11 00:15:10 -0700

    fixed js script link

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ var formEl = document.querySelector("#form");
 var quizIdCounter = 0;
 
 var quizStart = function() {
+    event.preventDefault();
+    
     console.log("fu");
 };
 
 formEl.addEventListener("submit", quizStart);
-console.log("fu");
