35fe661813117f167eed148a3960a2e2074e01d7
TylinnLaShay <tylinnhall@gmail.com>
2021-07-11 12:31:58 -0700

    testing quiz functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,43 @@
 var headerEl = document.querySelector("#header");
 var formEl = document.querySelector("#form");
 var quizIdCounter = 0;
+var questionEl = document.querySelector("#questions");
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
 
 var quizStart = function() {
     event.preventDefault();
-    
+
+    var quizQuestions = ["Question 1", "Question 2", "Question 3"];
+
     console.log("fu");
 };
 
