1bca0bb3992bbd1597a0f54df1bbf41206a693c6
TylinnLaShay <tylinnhall@gmail.com>
2021-07-12 01:54:00 -0700

    added correct answer counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
+//QUERY SELECTORS
 var headerEl = document.querySelector("#header");
 var formEl = document.querySelector("#form");
 var answerEl = document.querySelector("#answer");
 var startButtonEl = document.querySelector("#btn");
 var answerDivEl = document.querySelector("#answers");
+var questionEl = document.querySelector("#question");
+
+//COUNTERS
 var quizIdCounter = 0;
 var counterAnswer = 0;
-var questionEl = document.querySelector("#question");
+var clicks = 0;
+var correctCounter = 0;
+
+//ARRAY FOR QUESTIONS
 const myQuestions = [
   {
     question: "Who invented JavaScript?",
@@ -14,7 +21,7 @@ const myQuestions = [
       b: "Sheryl Sandberg",
       c: "Brendan Eich"
     },
-    correctAnswer: "c"
+    correctAnswer: "2"
   },
   {
     question: "Which one of these is a JavaScript package manager?",
@@ -23,7 +30,7 @@ const myQuestions = [
       b: "TypeScript",
       c: "npm"
     },
-    correctAnswer: "c"
+    correctAnswer: "2"
   },
   {
     question: "Which tool can you use to ensure code quality?",
@@ -32,40 +39,46 @@ const myQuestions = [
       b: "jQuery",
       c: "ESLint",
     },
-    correctAnswer: "c"
+    correctAnswer: "2"
   }
 ];
+
 // variable to store the list of possible answers
 const answers = [];
 const question = [];
+const correctAnswers = [];
 
 // for each question...
 myQuestions.forEach(
-  (currentQuestion, quizIdCounter) => {
+  (currentQuestion) => {
 
     // and for each available answer...
     for(letter in currentQuestion.answers){
       answers.push(currentQuestion.answers[letter]);
     }
-
-    // add this question and its answers to the output
+    //add question to output
     question.push(currentQuestion.question);
+    //add correct answer to the output
+    correctAnswers.push(currentQuestion.correctAnswer);
   }
 );
-
+//to remove previous buttons
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
 
 
 
 var generateQuestions = function() {
   event.preventDefault();
-
   if (quizIdCounter < myQuestions.length) {
         
     var newQuestion = question[quizIdCounter];
     questionEl.innerHTML = (newQuestion);
-
+    quizIdCounter++;
   }
-  quizIdCounter++;
   generateAnswers();
 }
 
@@ -74,27 +87,46 @@ var generateAnswers = function() {
 
     var buttonEl = document.createElement("button");
     var newAnswer = answers[counterAnswer];
+    var correct = correctAnswers[counterAnswer];
 
     //creating answer buttons
 
     buttonEl.innerHTML = (newAnswer);
     buttonEl.className = "btn";
-    buttonEl.setAttribute("data-task-id", quizIdCounter);
+    buttonEl.setAttribute("data-task-id", [i]);
     counterAnswer++;
     //appending each button to div
     answerDivEl.appendChild(buttonEl);
-  }
+  };
 };
+    //hopefully recording sometype of answer
+      document.body.addEventListener('click', function(e){
+      if(e.target.getAttribute("data-task-id") === correctAnswers[correctCounter]){
+      console.log('correct');
+      correctCounter++;
+    }
+    });
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
+var formEventHandler = function() {
+  if (quizIdCounter > 0) {
+    removeAllChildNodes(answerDivEl);
+    generateQuestions();
+  }
+  else {
+    document.querySelector("#btn").remove();
+    document.querySelector("#answer").remove();
+    generateQuestions();
   }
 }
 
+
+//ON CLICK START QUIZ
 formEl.addEventListener("submit", function() {
-  if (quizIdCounter > 0) {
-    removeAllChildNodes(answerDivEl);
+  event.preventDefault();
+  clicks++;
+  if (clicks > myQuestions.length + 1) {
+    console.log("Your score is" + correctCounter);
+
   }
-  generateQuestions();
+  formEventHandler();
 });
